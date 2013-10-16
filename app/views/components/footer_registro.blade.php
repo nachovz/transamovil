b63b34ac5a41c9d1656b393df46d1428e7b339cc
Klaha <klaha.77@gmail.com>
2013-10-16 11:12:13 -0430

    Footer de Registro2

--- app/views/components/footer_registro.blade.php
+++ app/views/components/footer_registro.blade.php
@@ -4,11 +4,11 @@
 <table width="100%" border="0" cellspacing="0" cellpadding="0">
   <tr>
     <td width="29%" height="29">&nbsp;</td>
-    <td width="10%"><a href="{{URL::route( 'quienessomos' )}}">QUIÉNES SOMOS</a></td>
+    <td width="10%">&nbsp;</td>
     <td width="6%">&nbsp;</td>
-    <td width="9%"><a href="{{URL::route( 'contactanos' )}}">CONTÁCTANOS</a></td>
+    <td width="15%"><a href="#">SUGERENCIAS Y RECLAMOS</a></td>
     <td width="4%">&nbsp;</td>
-    <td width="16%"><a href="{{URL::route( 'preguntas' )}}">PREGUNTAS FRECUENTES</a></td>
+    <td width="16%">&nbsp;</td>
     <td width="20%">&nbsp;</td>
     <td width="3%"><a href="https://www.facebook.com/TransaMovil" target="_blank" onmouseover="MM_swapImage('fb','','img/fb2.jpg',0)" onmouseout="MM_swapImgRestore()"><img src="img/fb.jpg" width="25" height="23" id="fb" border="0" style="display:block"/></a></td>
     <td width="3%"><a href="https://twitter.com/transamovil" target="_blank" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('tw','','img/tw2.jpg',0)"><img src="img/tw.jpg" width="25" height="23" id="tw" border="0" style="display:block"/></a></td>
