533b726d0148d812a9008e3eeb8875146c77e91d
Leonardo Graterol (Pankas) <pankas87@gmail.com>
2013-10-15 20:37:49 -0430

    Haciendo llamado al webservice

--- app/servicios/Servicio_digitel.php
+++ app/servicios/Servicio_digitel.php
@@ -1,5 +1,15 @@
 <?php 
 class Servicio_digitel extends Servicio
 {
-	
+	protected $url 				= 'http://digitel.transamovil.com/recargar.jsp';
+	protected $password			= 'transa';
+
+	//nro 04121000750'
+	public function pagar( $numero, $modo_pago, $monto )
+	{
+		$recarga_url = $this->url . '?telefono=' . $numero . '&paymentMode=' . $modo_pago . '&monto=' . $monto . '&password=' . $this->password;
+		echo '<pre>';
+		var_dump( file_get_contents( $recarga_url ) );
+		echo '</pre>';
+	}
 }
