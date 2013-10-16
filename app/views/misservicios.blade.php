d5726947d7e1aeeeef1dd60d89455efe6d1419a2
Klaha <klaha.77@gmail.com>
2013-10-16 12:32:24 -0430

    Ajustes nuevos v3

--- app/views/misservicios.blade.php
+++ app/views/misservicios.blade.php
@@ -6,14 +6,14 @@
 	<div id="mis_servicios"></div>
 	<div id="volver_home"><a href="{{URL::route( 'home' )}}"><img src="img/volver_home.png"></a></div>
 	<div class="servicio_texto_1">TODAVÍA NO TIENES NINGÚN SERVICIO AFILIADO, SELECCIONA EL SERVICIO QUE DESEAS AFILIAR</div>
-	<div class="servicio_texto_2">TELEFONÍA MÓVIL, FIJA E INTERNET</div>
+	<div class="servicio_texto_2" style="font-size:14px">TELEFONÍA MÓVIL, FIJA E INTERNET</div>
 	<div class="service_wrapper">
 		<div class="ai ia"><a href="{{URL::route( 'afiliaciondigitel' )}}"><img src="img/afiliacion_digitel.png"></a></div>
 		<div class="ai ia"></div>
 		<div class="ai ia"></div>
 		<div class="ai"></div>
 	</div>
-	<div class="servicio_texto_3">OTROS SERVICIOS</div>
+	<div class="servicio_texto_3" style="font-size:14px">OTROS SERVICIOS</div>
 	<div class="service_wrapper">
 		<div class="ai ia"></div>
 		<div class="ai ia"></div>
