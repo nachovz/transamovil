28de7d34523a79e5f8ea5e1931a4347e121b8e7c
Leonardo Graterol (Pankas) <pankas87@gmail.com>
2013-10-16 09:56:10 -0430

    PGSQL de MI$rDA

--- app/database/seeds/ServiciosTableSeeder.php
+++ app/database/seeds/ServiciosTableSeeder.php
@@ -7,8 +7,9 @@ class ServiciosTableSeeder extends Seeder {
 		// Uncomment the below to wipe the table clean before populating
 		// DB::table('servicios')->truncate();
 
+		$now = date('Y-m-d');
 		$servicios = array(
-			array( 'nombre' => 'Digitel', 'clase_servicio' => 'Servicio_digitel' )
+			array( 'nombre' => 'Digitel', 'clase_servicio' => 'Servicio_digitel', 'created_at' => $now, 'updated_at' => $now )
 		);
 
 		// Uncomment the below to run the seeder
