89d925a55ac7cdc351427d14313584c1c41499cc
Nacho <icordoba@stcsolutions.com.ve>
2013-11-07 14:58:06 -0430

    modal buttons if

--- app/views/digitelrecargaconfirmar.blade.php
+++ app/views/digitelrecargaconfirmar.blade.php
@@ -116,13 +116,8 @@
 	</div>
 	<div class="terms" style="display:none;"></div>	
 	<div class="buttons_6" style="top: 190px; width:494px">
-		<!--[if IE 8]> -->
+		<!-- <[if IE 8]> -->
 		<a href="{{URL::route( 'modal_recarga_confirmar', array( Input::get('monto'), Input::get('numero_afiliado'), Input::get('metodo_pago'), Input::get('numero_digitel') ? Input::get('numero_digitel') : '000' ) )}}"><img src="img/confirmar.png" class="img_space"></a>
-		<a href="{{URL::route( 'digitelrecarga' )}}"><img src="img/modificar.png" class="img_space"></a>
-		<a href="{{URL::route( 'home' )}}"><img src="img/cancelar_2.png"></a>
-		<div id="clave_pwd">
-		{{Form::open(array('method' => 'post')) . Form::password( 'password_check', array('id' => 'campo_registro', 'class' => 'campo_registro_pwd_check') )}}
-	</div>
 		<!--[endif]-->
 		<!--[if !IE]> -->
 		<a href="{{URL::route( 'modal_recarga_confirmar', array( Input::get('monto'), Input::get('numero_afiliado'), Input::get('metodo_pago'), Input::get('numero_digitel') ? Input::get('numero_digitel') : '000' ) )}}" class="clsVentanaIFrame3"><img src="img/confirmar.png" class="img_space"></a>
