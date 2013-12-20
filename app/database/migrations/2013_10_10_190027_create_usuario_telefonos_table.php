ca7e53dd874058c45e07b91f38f1e8e1ec4f8158
Abraham José González Barboza <dev@jgab.net>
2013-12-20 17:12:09 -0430

    changed

--- app/database/migrations/2013_10_10_190027_create_usuario_telefonos_table.php
+++ app/database/migrations/2013_10_10_190027_create_usuario_telefonos_table.php
@@ -17,6 +17,7 @@ class CreateUsuarioTelefonosTable extends Migration {
         $table->integer('usuario_id')->unsigned();
         $table->enum('tipo', array('Casa', 'Oficina', 'Celular'));
         $table->string('numero', 20);
+        $table->string('prefijo', 20)->nullable();
         $table->timestamps();
         //$table->primary(array('usuario_id', 'tipo', 'numero'));
         $table->foreign('usuario_id')->references('id')->on('usuarios')->onDelete('cascade')->onUpdate('cascade');
